1ab8ab2a912b5906df957fc8051a1b2b3510572d
Harshaditya Sharma <106009563+hercules2209@users.noreply.github.com>
2024-01-20 00:13:41 +0530

    v0.2.3.1 beta

--- src/components/Croprecommend.jsx
+++ src/components/Croprecommend.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { useMediaQuery } from 'react-responsive';
 import "./Croprecommend.css"
 
 function CropRecommendForm() {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const [result, setResult] = useState(""); // State to store the result
   const [isResultVisible, setIsResultVisible] = useState(false); // State to manage visibility
-
+  const isMobile = useMediaQuery({ maxWidth: 920 });
+  const onReset = () => {
+    setResult("");
+    setIsResultVisible(false);
+  };
   const onSubmit = (data) => {
     const formData = new FormData();
     formData.append('country', data.country);
@@ -18,41 +23,38 @@ function CropRecommendForm() {
     formData.append('nitrogenLevel', parseFloat(data.nitrogenLevel));
     formData.append('hydrogenLevel', parseFloat(data.hydrogenLevel));
 
-    // Log FormData to the console
-    console.log(formData);
 
     // Send FormData to http://localhost:5000/recommend
-    fetch('http://localhost:5000/recommend', {
+    fetch('https://us-central1-diseasedet.cloudfunctions.net/recommend_crop', {
       method: 'POST',
       body: formData,
     })
       .then(response => response.json())
       .then(data => {
         console.log('Recommendation response:', data);
-        setResult(JSON.stringify(data, null, 2)); // Store the result in state
+        setResult(data.details); // Store the result in state
         setIsResultVisible(true); // Show the result box
       })
       .catch(error => console.error('Error sending recommendation:', error));
   };
 
-  return (
-    <div className="min-h-screen p-6 flex items-center justify-center">
-      <div className="container max-w-screen-lg mx-auto">
-        <div>
-          <h2 className="font-semibold text-xl text-gray-600">Crop Recommendation Form</h2>
-          <p className="text-gray-500 mb-6">Please fill out all the fields regarding the location and soil data.</p>
-
-          <div className="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
-            <form onSubmit={handleSubmit(onSubmit)} className="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
-              <div className="text-gray-600">
-                <p className="font-medium text-lg">Location Details</p>
-                <p>Please provide the location details for the crop recommendation.</p>
-              </div>
-
-              <div className="lg:col-span-2">
-                <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
-                  {/* Location Inputs */}
-                  <div className="md:col-span-5">
+return (
+  <div className="min-h-screen flex items-center justify-center">
+    {/* Form Section */}
+    {!isResultVisible && (
+      <div className="container mx-auto"> {/* Remove max-w-screen-lg for full width on smaller screens */}
+        <div className="flex flex-col items-center justify-center py-4">
+          <h2 className="font-semibold text-xl  text-gray-900">Crop Recommendation Form</h2>
+          <p className="text-gray-700 mb-6 w-full">Please fill out all the fields regarding the location and soil data.</p>
+        </div>
+        <div className="bg-white rounded shadow-lg p-4 px-4 md:px-6"> {/* Adjust padding for smaller screens */}
+          <form onSubmit={handleSubmit(onSubmit)} className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-2"> {/* Use 2 columns on medium and up */}
+            {/* Location Details */}
+            <div className="bg-gray-50 p-4 rounded-lg mb-4">
+              <h3 className="font-medium text-lg">Location Details</h3>
+              <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-2"> {/* Use 2 columns on medium and up */}
+                {/* Location Inputs */}
+      <div className="md:col-span-5">
                     <label htmlFor="country">Country</label>
                     <input type="text" name="country" id="country" {...register('country', { required: true })} className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
                     {errors.country && <p className="text-red-600">Country is required.</p>}
@@ -75,54 +77,67 @@ function CropRecommendForm() {
                     <input type="text" name="zipcode" id="zipcode" {...register('zipcode', { required: true, pattern: /^[1-9]{1}[0-9]{5}$/ })} className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
                     {errors.zipcode && <p className="text-red-600">Zipcode is required and must be a valid format.</p>}
                   </div>
-                  </div>
-                  </div>
-                  
-                  {/* Soil Parameters Heading */}
-                  
-                  <div className="text-gray-600">
-                    <p className="font-medium text-lg">Soil Parameters</p>
-                    <p>Please fill in this information with the soil parameters from your soil health card or any recent soil health checkup for a personalized crop recommendation.</p>
-                    <p>**If you are not a farmer and do not have this information you can get simple soil checkup kits from our marketplace**</p>
-                  </div>
-                  <div className="lg:col-span-2">
-                  <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
-                  <div className="md:col-span-5">
+              </div>
+            </div>
+
+            {/* Soil Parameters Heading */}
+            <div className="bg-gray-50 p-4 rounded-lg mb-4">
+              <h3 className="font-medium text-lg">Soil Parameters</h3>
+              <p className="mb-4">Please fill in this information with the soil parameters from your soil health card or any recent soil health checkup for a personalized crop recommendation.</p>
+              <p className="mb-4">**If you are not a farmer and do not have this information you can get simple soil checkup kits from our marketplace**</p>
+              <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-2"> {/* Use 2 columns on medium and up */}
+                {/* Soil Inputs */}
+                <div className="md:col-span-5">
                     <label htmlFor="phLevel">pH Level (0-14)</label>
-                    <input type="number" name="phLevel" id="phLevel" {...register('phLevel', { required: true, min: 0, max: 14 })} className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
+                    <input type="number" name="phLevel" id="phLevel" step="0.01" {...register('phLevel', { required: true, min: 0, max: 14 })} className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
                     {errors.phLevel && <p className="text-red-600">pH level is required and must be between 0 and 14.</p>}
                   </div>
 
                   <div className="md:col-span-5">
                     <label htmlFor="potassiumLevel">Potassium Level</label>
-                    <input type="number" name="potassiumLevel" id="potassiumLevel" {...register('potassiumLevel', { required: true })} className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
+                    <input type="number" step="0.01" name="potassiumLevel" id="potassiumLevel" {...register('potassiumLevel', { required: true })} className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
                     {errors.potassiumLevel && <p className="text-red-600">Potassium level is required.</p>}
                   </div>
 
                   <div className="md:col-span-5">
                     <label htmlFor="nitrogenLevel">Nitrogen Level</label>
-                    <input type="number" name="nitrogenLevel" id="nitrogenLevel" {...register('nitrogenLevel', { required: true })} className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
+                    <input type="number" name="nitrogenLevel" id="nitrogenLevel" step="0.01" {...register('nitrogenLevel', { required: true })} className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
                     {errors.nitrogenLevel && <p className="text-red-600">Nitrogen level is required.</p>}
                   </div>
 
                   <div className="md:col-span-5">
                     <label htmlFor="hydrogenLevel">Hydrogen Level</label>
-                    <input type="number" name="hydrogenLevel" id="hydrogenLevel" {...register('hydrogenLevel', { required: true })} className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
+                    <input type="number" name="hydrogenLevel" id="hydrogenLevel" step="0.01" {...register('hydrogenLevel', { required: true })} className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
                     {errors.hydrogenLevel && <p className="text-red-600">Hydrogen level is required.</p>}
                   </div>
-                  </div>
-                  <div className="md:col-span-5 text-right">
-                    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+              </div>	
+            </div>
+            <div className="flex justify-end gap-4 py-4"> {/* Right-align buttons for smaller screens */}
+              <input type="reset" className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" />
+              <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+            </div>            
+            </form>    
+            </div>        
+          </div>
+            
+      
+            )}
+      {/* Output Section */}
+      {isResultVisible && (
+        <div className='result-container items-center justify-center h-screen'>
+          <div className="result-box rounded shadow-md px-6 md:px-10">
+            <h3 className="font-semibold text-xl text-gray-600">Recommendation Result</h3>
+            <div className='flex flex-col md:flex-row items-center justify-between'>
+              <div className='flex flex-col items-center md:items-start'>
+                {!isMobile && <img className='h-5 rounded-full md:h-10 md:rounded-xl' src="/Avatar.jpeg" alt="Avatar" />}
+                <div className='bg-white shadow-2xl rounded-3xl p-4'>
+                  <p className="text-left whitespace-pre-wrap text-sm">{result}</p>
+                  <p className='text-lg mt-3'>You can buy these at our marketplace: <a href="/market">Market</a></p>
                 </div>
               </div>
-            </form>
+              <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4 mb-4 md:mt-0 md:ml-4" onClick={onReset}>Retry</button>
+            </div>
           </div>
-        </div>
-      </div>
-      {isResultVisible && (
-        <div className="result-box bg-gray-100 p-4 rounded">
-          <h3 className="font-semibold text-xl text-gray-600">Recommendation Result</h3>
-          <pre className="text-gray-800">{result}</pre>
         </div>
       )}
     </div>
@@ -130,4 +145,4 @@ function CropRecommendForm() {
 }
 
 
-export default CropRecommendForm;
+export default CropRecommendForm;	  
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
+    fontSize: {
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+    },
     extend: {},
   },
   plugins: [],
