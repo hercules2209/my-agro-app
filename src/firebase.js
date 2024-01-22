e7a65506120b0e98420a892dd2fa61ff45d8867c
Harshaditya Sharma <106009563+hercules2209@users.noreply.github.com>
2024-01-22 19:12:28 +0530

    public bera v0.4

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { browserSessionPersistence } from "firebase/auth";
-import { getStorage } from 'firebase/storage'; // Import getStorage
+import { getStorage } from 'firebase/storage';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyArmxFwn3T2FGtMzyr065oF4MfehjVrTLw",
-  authDomain: "diseasedet.firebaseapp.com",
-  projectId: "diseasedet",
-  storageBucket: "diseasedet.appspot.com",
-  messagingSenderId: "1062062460810",
-  appId: "1:1062062460810:web:9b5a32738898c080f90273"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
+console.log(firebaseConfig);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 await auth.setPersistence(browserSessionPersistence);
-const storage = getStorage(app); // Initialize Firebase Storage
+const storage = getStorage(app);
 
-export { app, auth, storage }; // Export storage
+export { app, auth, storage };
